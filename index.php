5e1da6f7fb405fe42f19237005118440fa221e2d
Darshil Shah <shah.darshil23@yahoo.com>
2020-01-14 23:45:54 +0530

    a

--- index.php
+++ index.php
@@ -11,11 +11,11 @@
   <meta name="description" content="particles.js is a lightweight JavaScript library for creating particles.">
   <meta name="author" content="IEEE - SBM" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="temp/assets/css/style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  <script src="assets/js/randtext.js"></script>
+  <script src="temp/assets/js/randtext.js"></script>
 
 <script>
 $(document).ready(function(){
@@ -156,8 +156,8 @@ float: bottom;
 
 
 <!-- scripts -->
-<script src="assets/js/particles.min.js"></script>
-<script src="assets/js/app.js"></script>
+<script src="temp/assets/js/particles.min.js"></script>
+<script src="temp/assets/js/app.js"></script>
 
 </body>
 </html>
