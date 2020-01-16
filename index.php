f60d4d0da17d806e75891c50bfa30699e82ee261
Archit Gupta <rarchitgupta@gmail.com>
2020-01-16 23:13:50 +0530

    Edited index, event

--- index.php
+++ index.php
@@ -9,12 +9,14 @@
     
   </title>
   <meta name="description" content="particles.js is a lightweight JavaScript library for creating particles.">
+  <link href="https://fonts.googleapis.com/css?family=Sulphur+Point&display=swap" rel="stylesheet">
   <meta name="author" content="IEEE - SBM" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <link rel="stylesheet" href="temp/assets/css/style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+      
   <script src="temp/assets/js/randtext.js"></script>
 
 <script>
@@ -30,54 +32,64 @@ $(document).ready(function(){
 });
 </script>
 <style type="/text/css">
-  body , #particles-js {
-  width: 100%;
-  height: 100%;
-  color: #fff;
-  background: linear-gradient(-45deg, #000000, #3A3B3A, #0D1B5C, #020933);
-  background-size: 400% 400%;
-  -webkit-animation: Gradient 15s ease infinite;
-  -moz-animation: Gradient 15s ease infinite;
-  animation: Gradient 15s ease infinite;
-  z-index: 10;
-}
+  body , #particles-js 
+  {
+    width: 100%;
+    height: 100%;
+    color: #fff;
+    background: linear-gradient(-45deg, #000000, #3A3B3A, #0D1B5C, #020933);
+    background-size: 400% 400%;
+    -webkit-animation: Gradient 15s ease infinite;
+    -moz-animation: Gradient 15s ease infinite;
+    animation: Gradient 15s ease infinite;
+    z-index: 10;
+  }
 
-figure {
-  vertical-align: bottom;
-  align: center
-  display: inline;
-  text-align: bottom;
-  //width: 5px;
-  border: 0px dotted gray;
-  margin: 0px; /* adjust as needed */
-}
-figure img {
-  vertical-align: bottom;
-}
-figure figcaption {
-  vertical-align: middle;
-  text-align: center;
-  border: 0px dotted blue;
-  font-size: 30px;
-  font-family: sans-serif;
-}
+  figure 
+  {
+    vertical-align: bottom;
+    align: center
+    display: inline;
+    text-align: bottom;
+    //width: 5px;
+    border: 0px dotted gray;
+    margin: 0px; /* adjust as needed */
+  }
+  
+  figure img 
+  {
+    vertical-align: bottom;
+  }
+  
+  figure figcaption 
+  {
+    vertical-align: middle;
+    text-align: center;
+    border: 0px dotted blue;
+    font-size: 30px;
+    font-family: sans-serif;
+  }
 
-footer {
+  footer 
+  {
     background: #444444;
     height: 200px;
     font-family: 'Open Sans', sans-serif;
     color: #FFFFFF;
     padding: 200px;
-}
+  }
 
-.pic {
-float: bottom;
-}
-.fix{
+  .pic 
+  {
+    float: bottom;
+  }
+  
+  .fix
+  {
     position:fixed;
     bottom:5px;
     left:29%;
-}
+   }
 
 @-webkit-keyframes Gradient {
   0% {
