0fdd71c0703beb1cb2eb354ee26edd72a5984b40
Darshil Shah <shah.darshil23@yahoo.com>
2020-01-29 19:33:13 +0530

    Black Flag

--- registerblackflag.php
+++ registerblackflag.php
@@ -31,7 +31,7 @@
 	<div class="login">
 	<form method="post" action="teamblackflag.php" name="regform">
 	<h1 id="register">Register</h1><br>
-		<p id="para">Name </p><input type="text" name="team" id="name" ><br><br>
+		<p id="para">Name </p><input type="text" name="name" id="name" ><br><br>
 		<p id="para">Id</p><input type="number" name="id" min="1" id="number"><br><br>
 		<div class="g-recaptcha" data-sitekey="6LcUks4UAAAAAIL-fzoauiSN0H59bTa6vrmzENm8"></div><br><br>
 		<input type="submit" value = "Submit" name="submit" >
